d3a957b2668129c3a0867e784c58c8429b242105
dragonscry <denismerkotun@gmail.com>
2025-05-19 19:10:33 +0300

    Init project

---
+++ testSpaceX/testSpaceX/testSpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSpaceXApp.swift
+//  testSpaceX
+//
+//  Created by Денис Меркотун on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct testSpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
